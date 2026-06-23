1ab54774e68fdc48de1a43166bbad7c85bfbc1b4
Abraham <abraao.matos236@gmail.com>
2026-06-22 21:57:57 -0400

    feat: refactor theme provider implementation and update imports

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import { ThemeProviderProps } from "next-themes";
+import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
+export function ThemeProvider({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
-};
-
-export default ThemeProvider;
+}
