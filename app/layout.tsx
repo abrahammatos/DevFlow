0f59f2253c2a4f61cf23bb9667211951ea4df076
Abraham <abraao.matos236@gmail.com>
2026-06-22 21:49:22 -0400

    feat: add new UI components and utilities
    - Updated package.json to include new dependencies: class-variance-authority, clsx, lucide-react, radix-ui, shadcn, tailwind-merge, and tw-animate-css.
    - Created components.json for component configuration with shadcn.
    - Implemented Navbar component for navigation.
    - Developed Button component with variants and sizes using class-variance-authority.
    - Added DropdownMenu component with various subcomponents for enhanced dropdown functionality.
    - Introduced utility function `cn` for merging class names using clsx and tailwind-merge.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ThemeProvider from "@/context/Theme";
+import Navbar from "@/components/navigation/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,9 +29,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-      <body className="flex min-h-full flex-col" suppressHydrationWarning>
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
+    >
+      <body className="flex min-h-full flex-col">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          <Navbar />
           {children}
         </ThemeProvider>
       </body>
