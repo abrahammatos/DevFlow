3ace5d6e41d6b3c61fd1401ec84227e6cd43fdba
Abraham <abraao.matos236@gmail.com>
2026-09-07 22:19:47 -0400

    feat: integrate MDX editor with dark theme support and update dependencies
    - Added dark-editor.css for dark theme styling using Radix UI colors.
    - Implemented MDXEditor component with various plugins including code block support and toolbar functionalities.
    - Updated package.json to include @mdxeditor/editor and cm6-theme-basic-dark for enhanced editor capabilities.
    - Incremented @tailwindcss/typography version to 0.5.20.

---
+++ components/editor/index.tsx
@@ -0,0 +1,130 @@
+"use client";
+import type { ForwardedRef } from "react";
+import {
+  headingsPlugin,
+  listsPlugin,
+  quotePlugin,
+  thematicBreakPlugin,
+  markdownShortcutPlugin,
+  MDXEditor,
+  type MDXEditorMethods,
+  toolbarPlugin,
+  ConditionalContents,
+  ChangeCodeMirrorLanguage,
+  UndoRedo,
+  Separator,
+  BoldItalicUnderlineToggles,
+  ListsToggle,
+  InsertImage,
+  CreateLink,
+  InsertTable,
+  InsertThematicBreak,
+  InsertCodeBlock,
+  linkPlugin,
+  linkDialogPlugin,
+  tablePlugin,
+  codeBlockPlugin,
+  codeMirrorPlugin,
+  diffSourcePlugin,
+} from "@mdxeditor/editor";
+import { basicDark } from "cm6-theme-basic-dark";
+import "@mdxeditor/editor/style.css";
+import "./dark-editor.css";
+import { useTheme } from "next-themes";
+
+interface Props {
+  value: string;
+  fieldChange: (value: string) => void;
+  editorRef: ForwardedRef<MDXEditorMethods> | null;
+}
+const Editor = ({ value, fieldChange, editorRef, ...props }: Props) => {
+  const { resolvedTheme } = useTheme();
+  const theme = resolvedTheme === "dark" ? [basicDark] : [];
+
+  return (
+    <MDXEditor
+      key={resolvedTheme}
+      markdown={value}
+      className="background-light800_dark200 light-border-2 markdown-editor dark-editor grid w-full border"
+      onChange={fieldChange}
+      plugins={[
+        // Example Plugin Usage
+        headingsPlugin(),
+        listsPlugin(),
+        linkPlugin(),
+        linkDialogPlugin(),
+        quotePlugin(),
+        thematicBreakPlugin(),
+        markdownShortcutPlugin(),
+        tablePlugin(),
+        codeBlockPlugin({ defaultCodeBlockLanguage: "" }),
+        codeMirrorPlugin({
+          codeBlockLanguages: {
+            css: "css",
+            js: "javascript",
+            ts: "typescript",
+            html: "html",
+            json: "json",
+            sql: "sql",
+            txt: "text",
+            sass: "sass",
+            scss: "scss",
+            bash: "bash",
+            sh: "bash",
+            shell: "bash",
+            yml: "yaml",
+            yaml: "yaml",
+            md: "markdown",
+            "": "unspecified",
+            tsx: "typescript (React)",
+            jsx: "javascript (React)",
+          },
+          autoLoadLanguageSupport: true,
+          codeMirrorExtensions: theme,
+        }),
+        diffSourcePlugin({ viewMode: "rich-text", diffMarkdown: "" }),
+        toolbarPlugin({
+          toolbarContents: () => {
+            return (
+              <ConditionalContents
+                options={[
+                  {
+                    when: (editor) => editor?.editorType === "codeblock",
+                    contents: () => <ChangeCodeMirrorLanguage />,
+                  },
+                  {
+                    fallback: () => (
+                      <>
+                        <UndoRedo />
+                        <Separator />
+
+                        <BoldItalicUnderlineToggles />
+                        <Separator />
+
+                        <ListsToggle />
+                        <Separator />
+
+                        <CreateLink />
+                        <InsertImage />
+                        <Separator />
+
+                        <InsertTable />
+                        <InsertThematicBreak />
+
+                        <InsertCodeBlock />
+                      </>
+                    ),
+                  },
+                ]}
+              />
+            );
+          },
+        }),
+      ]}
+      {...props}
+      ref={editorRef}
+    />
+  );
+};
+
+export default Editor;
--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -6,9 +6,20 @@ import { Controller, useForm } from "react-hook-form";
 import { FieldGroup, Field, FieldLabel, FieldError, FieldDescription } from "../ui/field";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import { useRef } from "react";
+import { MDXEditorMethods } from "@mdxeditor/editor";
+import dynamic from "next/dynamic";
+import z from "zod";
+import TagCard from "../cards/TagCard";
+
+const Editor = dynamic(() => import("@/components/editor"), {
+  ssr: false,
+});
 
 const QuestionForm = () => {
-  const form = useForm({
+  const editorRef = useRef<MDXEditorMethods | null>(null);
+
+  const form = useForm<z.infer<typeof AskQuestionSchema>>({
     resolver: zodResolver(AskQuestionSchema),
     defaultValues: {
       title: "",
@@ -17,8 +28,44 @@ const QuestionForm = () => {
     },
   });
 
-  const handleCreateQuestion = async () => {
-    console.log();
+  const handleInputKeyDown = (e: React.KeyboardEvent<HTMLInputElement>, field: { value: string[] }) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      const tagInput = e.currentTarget.value.trim();
+
+      if (tagInput && tagInput.length < 15 && !field.value.includes(tagInput)) {
+        form.setValue("tags", [...field.value, tagInput]);
+        e.currentTarget.value = "";
+        form.clearErrors("tags");
+      } else if (tagInput.length > 15) {
+        form.setError("tags", {
+          type: "manual",
+          message: "Tag should be less than 15 characters",
+        });
+      } else if (field.value.includes(tagInput)) {
+        form.setError("tags", {
+          type: "manual",
+          message: "Tag already exists",
+        });
+      }
+    }
+  };
+
+  const handleTagRemove = (tag: string, field: { value: string[] }) => {
+    const newTags = field.value.filter((t) => t !== tag);
+
+    form.setValue("tags", newTags);
+
+    if (newTags.length === 0) {
+      form.setError("tags", {
+        type: "manual",
+        message: "Tags are required",
+      });
+    }
+  };
+
+  const handleCreateQuestion = (data: z.infer<typeof AskQuestionSchema>) => {
+    console.log(data);
   };
 
   return (
@@ -57,7 +104,7 @@ const QuestionForm = () => {
               <FieldLabel htmlFor={field.name} className="paragraph-semibold text-dark400_light800">
                 Detailed explanation of your problem <span className="text-primary-500">*</span>
               </FieldLabel>
-              editor
+              <Editor value={field.value} editorRef={editorRef} fieldChange={field.onChange} />
               {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
               <FieldDescription className="body-regular text-light-500 mt-2.5">
                 Introduce the problem and expand on what you&apos;ve put in the title.
@@ -81,9 +128,23 @@ const QuestionForm = () => {
                   aria-invalid={fieldState.invalid}
                   placeholder="Add tags..."
                   className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 no-focus min-h-14 border"
-                  {...field}
+                  onKeyDown={(e) => handleInputKeyDown(e, field)}
                 />
-                tags
+                {field.value.length > 0 && (
+                  <div className="flex-start mt-2.5 flex-wrap gap-2.5">
+                    {field?.value?.map((tag: string) => (
+                      <TagCard
+                        key={tag}
+                        _id={tag}
+                        name={tag}
+                        compact
+                        remove
+                        isButton
+                        handleRemove={() => handleTagRemove(tag, field)}
+                      />
+                    ))}
+                  </div>
+                )}
                 {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
               </div>
               <FieldDescription className="body-regular text-light-500 mt-2.5">
